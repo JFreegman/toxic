40b79f5e6947f665a9c9835e223e8894356806f1
Jfreegman <Jfreegman@gmail.com>
2013-11-15 19:35:14 -0500

    consistent formatting for prototypes

--- src/global_commands.h
+++ src/global_commands.h
@@ -2,14 +2,14 @@
  * Toxic -- Tox Curses Client
  */
 
-void cmd_accept(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_add(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_clear(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_connect(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_groupchat(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_myid(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_nick(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_note(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_prompt_help(WINDOW *window, ToxWindow *prompt, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_quit(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
-void cmd_status(WINDOW *, ToxWindow *, Tox *m, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_accept(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_add(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_clear(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_connect(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_groupchat(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_myid(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_nick(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_note(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_prompt_help(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_quit(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
+void cmd_status(WINDOW *, ToxWindow *, Tox *, int num, int argc, char (*argv)[MAX_STR_SIZE]);
