fd86f01fd0a0cf350d698e679cc93d2a0ff71861
mannol <eniz_vukovic@hotmail.com>
2014-02-23 00:00:34 +0100

    Started with audio

---
+++ src/audio_call.c
@@ -0,0 +1,14 @@
+/*
+ * Toxic -- Tox Curses Client
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef _SUPPORT_AUDIO
+#include <tox/toxav.h>
+
+#error "Get me a kill"
+
+#endif /* _SUPPORT_AUDIO */
---
+++ src/audio_call.h
@@ -0,0 +1,8 @@
+/*
+ * Toxic -- Tox Curses Client
+ */
+
+#ifndef _audio_h
+#define _audio_h
+
+#endif /* _audio_h */
