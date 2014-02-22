aec16a22a454bf153bfa34890edb3dab01a050ec
Sean <sean@tox.im>
2014-02-22 01:39:32 -0800

    Update prompt.c

--- src/prompt.c
+++ src/prompt.c
@@ -418,7 +418,7 @@ void prompt_init_statusbar(ToxWindow *self, Tox *m)
     snprintf(statusbar->nick, sizeof(statusbar->nick), "%s", nick);
 
     /* temporary until statusmessage saving works */
-    uint8_t *statusmsg = "Toxing on Toxic v.0.2.4";
+    uint8_t *statusmsg = "Toxing on Toxic v.0.2.5-git";
     m_set_statusmessage(m, statusmsg, strlen(statusmsg) + 1);
     snprintf(statusbar->statusmsg, sizeof(statusbar->statusmsg), "%s", statusmsg);
 
