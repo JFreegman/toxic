9581940cfaae697ccaa217644b5f4248b93d6486
Ansa89 <ansalonistefano@gmail.com>
2014-08-26 10:39:43 +0200

    Cosmetic fixes

--- cfg/targets/doc.mk
+++ cfg/targets/doc.mk
@@ -4,7 +4,7 @@ doc: $(MANFILES:%=$(DOC_DIR)/%)
 $(DOC_DIR)/%: $(DOC_DIR)/%.asc
 	@echo "  MAN   $(@F)"
 	@a2x -f manpage -a revdate=$(shell git log -1 --date=short --format="%ad" $<) \
-		-a manmanual="toxic manual" -a mansource=toxic \
+		-a manmanual="Toxic Manual" -a mansource=toxic \
 		-a manversion=__VERSION__ -a datadir=__DATADIR__ $<
 	
 .PHONY: doc
