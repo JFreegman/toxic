bbb639c5aa8a621dce5a280b14974529d1802142
Ansa89 <ansalonistefano@gmail.com>
2015-04-03 13:04:28 +0200

    Makefile: add uninstall target

---
+++ cfg/targets/uninstall.mk
@@ -0,0 +1,24 @@
+# Uninstall target
+uninstall:
+	@echo "Removing toxic executable"
+	@rm -f $(abspath $(DESTDIR)/$(BINDIR)/toxic)
+	
+	@echo "Removing desktop file"
+	@rm -f $(abspath $(DESTDIR)/$(APPDIR)/$(DESKFILE))
+	
+	@echo "Removing data files"
+	@for f in $(DATAFILES) ; do \
+		rm -f $(abspath $(DESTDIR)/$(DATADIR)/$$f) ;\
+	done
+	@for f in $(SNDFILES) ; do \
+		rm -f $(abspath $(DESTDIR)/$(DATADIR)/sounds/$$f) ;\
+	done
+	
+	@echo "Removing man pages"
+	@for f in $(MANFILES) ; do \
+		section=$(abspath $(DESTDIR)/$(MANDIR))/man`echo $$f | rev | cut -d "." -f 1` ;\
+		file=$$section/$$f ;\
+		rm -f $$file $$file.gz ;\
+	done
+
+.PHONY: uninstall
