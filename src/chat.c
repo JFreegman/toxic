06c268417fba374087bc7466f7393794dfa3d4f2
Jfreegman <jfreegman@gmail.com>
2015-06-30 22:40:45 -0400

    Fix some possible race conditions related to line printing

--- src/chat.c
+++ src/chat.c
@@ -928,7 +928,10 @@ static void chat_onDraw(ToxWindow *self, Tox *m)
 
     ChatContext *ctx = self->chatwin;
 
+    pthread_mutex_lock(&Winthread.lock);
     line_info_print(self);
+    pthread_mutex_unlock(&Winthread.lock);
+
     wclear(ctx->linewin);
 
     curs_set(1);
