c755247434490fe407999ac3289c0ed4026e8b93
cnhenry <h724n828@gmail.com>
2015-07-10 16:42:45 -0500

    Added video_device.h file

--- cfg/checks/av.mk
+++ cfg/checks/av.mk
@@ -7,6 +7,15 @@ else
     AUDIO_OBJ = audio_call.o audio_device.o
 endif
 
+# Variables for video call support
+VIDEO_LIBS = libtoxav opencv
+AUDIO_CFLAGS = -DVIDEO
+ifneq (, $(findstring video_device.o $(OBJ)))
+    VIDEO_OBJ = video_call.o
+else
+    VIDEO_OBJ = video_call.o video_device.o
+endif
+
 # Check if we can build audio support
 CHECK_AUDIO_LIBS = $(shell $(PKG_CONFIG) --exists $(AUDIO_LIBS) || echo -n "error")
 ifneq ($(CHECK_AUDIO_LIBS), error)
@@ -19,3 +28,15 @@ else ifneq ($(MAKECMDGOALS), clean)
     $(warning WARNING -- You need these libraries for audio support)
     $(warning WARNING -- $(MISSING_AUDIO_LIBS))
 endif
+
+# Check if we can build video support
+CHECK_VIDEO_LIBS = $(shell pkg-config --exists $(VIDEO_LIBS) || echo -n "error")
+ifneq ($(CHECK_VIDEO_LIBS), error)
+    LIBS += $(VIDEO_LIBS)
+    CFLAGS += $(VIDEO_CFLAGS)
+    OBJ += $(VIDEO_OBJ)
+else ifneq ($(MAKECMDGOALS), clean)
+    MISSING_VIDEO_LIBS = $(shell for lib in $(VIDEO_LIBS) ; do if ! pkg-config --exists $$lib ; then echo $$lib ; fi ; done)
+    $(warning WARNING -- Toxic will be compiled without video support)
+    $(warning WARNING -- You will need these libraries for video support)
+    $(warning WARNING -- $(MISSING_VIDEO_LIBS))
---
+++ src/video_device.h
@@ -0,0 +1,21 @@
+/*  video_device.h
+ *
+ *
+ *  Copyright (C) 2014 Toxic All Rights Reserved.
+ *
+ *  This file is part of Toxic.
+ *
+ *  Toxic is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Toxic is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Toxic.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
