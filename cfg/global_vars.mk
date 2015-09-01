ffcc804efe84bc4285f5fcc34a8f301fd40fa8d4
Jfreegman <jfreegman@gmail.com>
2015-09-01 13:00:56 -0400

    Moved version up

--- cfg/global_vars.mk
+++ cfg/global_vars.mk
@@ -1,5 +1,5 @@
 # Version
-TOXIC_VERSION = 0.6.0
+TOXIC_VERSION = 0.6.1
 REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
 ifneq (, $(findstring error, $(REV)))
     VERSION = $(TOXIC_VERSION)
