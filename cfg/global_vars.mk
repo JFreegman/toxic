3917f664bfcaa358ad92436db1317afbfe918d16
jfreegman <jfreegman@gmail.com>
2018-03-01 19:14:14 -0500

    Bump to v0.8.2

--- cfg/global_vars.mk
+++ cfg/global_vars.mk
@@ -1,5 +1,5 @@
 # Version
-TOXIC_VERSION = 0.8.1
+TOXIC_VERSION = 0.8.2
 REV = $(shell git rev-list HEAD --count 2>/dev/null || echo -n "error")
 ifneq (, $(findstring error, $(REV)))
     VERSION = $(TOXIC_VERSION)
