0212391d7db98e40d612b20327741adb2aa5886c
jfreegman <jfreegman@gmail.com>
2021-11-08 16:01:52 -0500

    Disable DHT nodeslist fetching temporarily
    We don't want to fetch the mainnet nodes list when we're on
    the NGC testnet. This should be fixed before the NGC merge

--- src/bootstrap.c
+++ src/bootstrap.c
@@ -42,7 +42,7 @@
 extern struct user_settings *user_settings;
 
 /* URL that we get the JSON encoded nodes list from. */
-#define NODES_LIST_URL "https://digcoin.network/random/NGC-bootstrap-nodes.json"
+#define NODES_LIST_URL 0  // This should be empty until NGC merges with mainnet
 
 #define DEFAULT_NODES_FILENAME "DHTnodes.json"
 
@@ -276,6 +276,11 @@ on_exit:
  */
 static int update_DHT_nodeslist(const char *nodes_path)
 {
+    if (NODES_LIST_URL == 0) {  // TODO: Remove this when NGC merges with mainnet
+        fprintf(stderr, "Skipping DHT Nodes list fetching\n");
+        return 0;
+    }
+
     if (!nodeslist_needs_update(nodes_path)) {
         return 0;
     }
